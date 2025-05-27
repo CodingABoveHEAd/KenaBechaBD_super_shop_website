b0187d73d285fffd4abb9f7e7c9dfed5cc618643
niloy <chowdhuryniloy819@gmail.com>
2025-05-27 15:48:16 +0600

    fixed admin panel

--- kenabechabd-ecommerce-backend/middlewares/authUser.js
+++ kenabechabd-ecommerce-backend/middlewares/authUser.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 export const authUser = async (req, res, next) => {
   const { token,sellertoken } = req.cookies;
-  if(sellertoken)return;
+  // if(sellertoken)return;
   if (!token) {
     return res.status(400).json({ success: false, message: "Not authorized. Signup or Login to make purchase" });
   }
